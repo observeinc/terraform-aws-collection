4dbc57474c738f226a9cdfac86824df19fa3d07a
Luke Zhu <luke@observeinc.com>
2022-06-22 23:07:37 +0000

    feat: use new log group subscription module

--- firehose.tf
+++ firehose.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "group" {
+resource "aws_cloudwatch_log_group" "firehose" {
   name              = format("/aws/firehose/%s", var.name)
   retention_in_days = var.retention_in_days
 }
@@ -14,7 +14,7 @@ module "observe_kinesis_firehose" {
   iam_name_prefix                  = local.name_prefix
   s3_delivery_bucket               = local.s3_bucket
   http_endpoint_buffering_interval = 60
-  cloudwatch_log_group             = aws_cloudwatch_log_group.group
+  cloudwatch_log_group             = aws_cloudwatch_log_group.firehose
   tags                             = var.tags
 }
 
@@ -27,11 +27,3 @@ module "observe_cloudwatch_metrics" {
   exclude_filters  = var.cloudwatch_metrics_exclude_filters
 }
 
-module "observe_firehose_cloudwatch_logs_subscription" {
-  source           = "github.com/observeinc/terraform-aws-kinesis-firehose?ref=v0.4.0//cloudwatch_logs_subscription"
-  kinesis_firehose = module.observe_kinesis_firehose
-  iam_name_prefix  = local.name_prefix
-  log_group_names = concat(local.subscribed_log_group_names_firehose, [
-    format("/aws/lambda/%s", var.name)
-  ])
-}
---
+++ subscription.tf
@@ -0,0 +1,20 @@
+module "observe_cloudwatch_logs_subscription" {
+  source           = "github.com/observeinc/terraform-aws-cloudwatch-logs-subscription?ref=v0.2.0"
+  kinesis_firehose = module.observe_kinesis_firehose
+  iam_name_prefix  = local.name_prefix
+
+  filter_name    = var.name
+  filter_pattern = var.subscribed_log_group_filter_pattern
+
+  log_group_matches = concat(
+    [aws_cloudwatch_log_group.firehose.name, format("/aws/lambda/%s", var.name)], # Note: Data from firehose will go to itself. There is a cycle.
+    var.subscribed_log_group_matches,
+    local.subscribed_log_group_names_firehose,
+  )
+  log_group_excludes = var.subscribed_log_group_excludes
+
+  # avoid racing with s3 bucket subscription
+  depends_on = [
+    module.observe_lambda_s3_bucket_subscription
+  ]
+}
