a50fd397048237f9a5221f48c83b646b1cc050bd
João Taveira Araújo <joao@observeinc.com>
2023-02-28 15:01:39 -0800

    fix!: update cloudwatch logs subscription module
    This commit bumps the cloudwatch logs subscription module and gates it
    conditionally on the `subscribed_log_group_matches` and
    `subscribed_log_group_excludes` variables. If neither list is provided,
    the module will not be installed.
    
    As a result of these changes, the minimum terraform provider version is
    now 1.1.0. This is matches the requirements of the underyling cloudwatch
    logs subscription module, and allows the usage of `moved` directive to
    retain existing cloudwatch log subscription modules across upgrades.

--- subscription.tf
+++ subscription.tf
@@ -1,6 +1,16 @@
+locals {
+  enable_subscription = length(var.subscribed_log_group_matches) + length(var.subscribed_log_group_excludes) > 0
+}
+
+moved {
+  from = module.observe_cloudwatch_logs_subscription
+  to   = module.observe_cloudwatch_logs_subscription[0]
+}
+
 module "observe_cloudwatch_logs_subscription" {
+  count   = local.enable_subscription ? 1 : 0
   source  = "observeinc/cloudwatch-logs-subscription/aws"
-  version = "0.4.0"
+  version = "0.5.0"
 
   name             = var.log_subscription_name
   kinesis_firehose = module.observe_kinesis_firehose
@@ -9,11 +19,12 @@ module "observe_cloudwatch_logs_subscription" {
   filter_name    = var.name
   filter_pattern = var.subscribed_log_group_filter_pattern
 
-  log_group_matches = concat(
-    [aws_cloudwatch_log_group.group.name, format("/aws/lambda/%s", var.name)], # Note: Data from firehose will go to itself. There is a cycle.
-    var.subscribed_log_group_matches,
+  log_group_matches = var.subscribed_log_group_matches
+  log_group_excludes = concat(
+    var.subscribed_log_group_excludes,
+    # log groups managed by our module should be explicitly subscribed
+    [aws_cloudwatch_log_group.group.name, "/aws/lambda/${var.name}"],
   )
-  log_group_excludes = var.subscribed_log_group_excludes
 
   # avoid racing with s3 bucket subscription
   depends_on = [
