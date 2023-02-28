0758ee6e18f31566e2b69743ffd428b56f88bdee
João Taveira Araújo <joao@observeinc.com>
2023-02-28 14:43:34 -0800

    fix: bump kinesis firehose module to 1.0.3 (#44)
    Latest module wires in tags.

--- eventbridge.tf
+++ eventbridge.tf
@@ -4,15 +4,17 @@ resource "aws_cloudwatch_event_rule" "wildcard" {
   event_pattern = jsonencode({
     "account" : [data.aws_caller_identity.current.account_id]
   })
+  tags = var.tags
 }
 
 module "observe_firehose_eventbridge" {
   source  = "observeinc/kinesis-firehose/aws//modules/eventbridge"
-  version = "1.0.2"
+  version = "1.0.3"
 
   kinesis_firehose = module.observe_kinesis_firehose
   iam_name_prefix  = local.name_prefix
   rules = [
     aws_cloudwatch_event_rule.wildcard,
   ]
+  tags = var.tags
 }
