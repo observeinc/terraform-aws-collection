9e2220176e47e6b7485eb8591f7c1cf36e7e1531
João Taveira Araújo <joao@observeinc.com>
2024-03-04 11:30:13 -0800

    fix(testing): remove subscriptions module, use bucket_prefix
    We don't reference the subscriptions module in this repo, so we should
    move it closer to the point of use since there is only one consumer
    today.

--- modules/testing/s3_bucket/kms.tf
+++ modules/testing/s3_bucket/kms.tf
@@ -3,9 +3,3 @@ resource "aws_kms_key" "this" {
   enable_key_rotation = true
   policy              = var.kms_key_policy_json
 }
-
-resource "aws_kms_alias" "this" {
-  count         = length(aws_kms_key.this)
-  name          = "alias/${var.setup.short}"
-  target_key_id = aws_kms_key.this[0].key_id
-}
--- modules/testing/subscriptions/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "buckets" {
-  description = "Buckets by source"
-  value       = aws_s3_bucket.source
-}
