71cac604b620174ce2d50a154c7491c864b8f322
João Taveira Araújo <joao@observeinc.com>
2024-03-12 11:16:56 -0700

    fix(logwriter): adjust prefixes (#129)
    Adjust prefixes to match those of other AWS services.

--- modules/logwriter/kinesis.tf
+++ modules/logwriter/kinesis.tf
@@ -5,8 +5,8 @@ resource "aws_kinesis_firehose_delivery_stream" "delivery_stream" {
   extended_s3_configuration {
     role_arn            = aws_iam_role.firehose.arn
     bucket_arn          = var.bucket_arn
-    prefix              = var.prefix
-    error_output_prefix = var.prefix
+    prefix              = "${var.prefix}AWSLogs/${local.account_id}/cloudwatchlogs/${local.region}/"
+    error_output_prefix = "${var.prefix}AWSLogs/${local.account_id}/cloudwatchlogs/${local.region}/errors/"
     buffering_interval  = var.buffering_interval
     buffering_size      = var.buffering_size
     cloudwatch_logging_options {
