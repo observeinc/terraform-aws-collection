835db78c392fc79f2b947c74616b2dfa769347b6
João Taveira Araújo <joao@observeinc.com>
2024-04-26 10:25:28 -0700

    fix(stack): introduce configsubscription variable
    This commit addresses a limitation of our current module API whereby
    passing a bucket reference would raise an error, e.g.:
    
    ```
    module "forwarder" {
      ...
      config_delivery_bucket_name = aws_s3_bucket.this.id
    }
    ```
    
    would trigger:
    ```
    │ Error: Invalid count argument
    │
    │   on .terraform/modules/forwarder/modules/stack/configsubscription.tf line 2, in module "configsubscription":
    │    2:   count  = var.config_delivery_bucket_name != null ? 1 : 0
    │
    │ The "count" value depends on resource attributes that cannot be determined until apply, so Terraform cannot predict how many instances will be created. To work around this, use the -target argument to
    │ first apply only the resources that the count depends on.
    ```
    
    To work around this, we can instead gate the condition on the
    presence of an object, which is the pattern we follow for all other
    nested modules:
    
    ```
    module "forwarder" {
      ...
      configsubscription = {
        delivery_bucket_name = aws_s3_bucket.this.id
      }
    }
    ```
    
    In this case, we no longer need to worry about whether
    `delivery_bucket_name` is known, since our condition will verify the
    presence of `configsubscription` instead.

--- modules/stack/configsubscription.tf
+++ modules/stack/configsubscription.tf
@@ -1,5 +1,5 @@
 module "configsubscription" {
-  count  = var.config_delivery_bucket_name != "" ? 1 : 0
+  count  = var.configsubscription != null ? 1 : 0
   source = "../configsubscription"
 
   name_prefix = local.name_prefix
