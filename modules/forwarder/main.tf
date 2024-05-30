dd8347dffa3056ee99f3d314a546d54a18faa25d
João Taveira Araújo <joao@observeinc.com>
2024-05-30 11:01:33 -0700

    fix(forwarder): relax IAM role naming (#160)
    The IAM role of the forwarder only needs to be known when forwarding
    data to Filedrop. For cases where we are not pointing at an S3 data
    access point, we can fallback to provisioning a role with `name_prefix`
    rather than `name`. Doing so avoids the possibility of name clashes when
    installing a forwarder in different regions.

--- modules/forwarder/main.tf
+++ modules/forwarder/main.tf
@@ -2,6 +2,7 @@ locals {
   s3_uri          = one([for item in csvdecode(file("${path.module}/uris.csv")) : item["code_uri"] if item["region"] == data.aws_region.current.name])
   parsed_s3_uri   = regex("s3://(?P<bucket>[^/]+)/(?P<key>.+)", local.s3_uri)
   destination_uri = var.destination.uri != "" ? var.destination.uri : "s3://${var.destination.bucket}/${var.destination.prefix}"
+  name_prefix     = "${substr(var.name, 0, 37)}-"
 
   default_limits = startswith(local.destination_uri, "s3") ? {
     memory_size   = 128
