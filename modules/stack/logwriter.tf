a0511e91b119e859027e1308e5f02cbe1e334f17
Conventional Changelog Action <conventional.changelog.action@github.com>
2025-08-25 15:48:01 +0000

    chore(release): v2.28.0 [skip ci]

--- modules/stack/logwriter.tf
+++ modules/stack/logwriter.tf
@@ -5,22 +5,23 @@ module "logwriter" {
   name       = "${var.name}-logwriter"
   bucket_arn = aws_s3_bucket.this.arn
 
-  log_group_name_patterns = var.logwriter.log_group_name_patterns
-  log_group_name_prefixes = var.logwriter.log_group_name_prefixes
-  buffering_interval      = var.logwriter.buffering_interval
-  buffering_size          = var.logwriter.buffering_size
-  filter_name             = var.logwriter.filter_name
-  filter_pattern          = var.logwriter.filter_pattern
-  num_workers             = var.logwriter.num_workers
-  discovery_rate          = var.logwriter.discovery_rate
-  lambda_memory_size      = var.logwriter.lambda_memory_size
-  lambda_timeout          = var.logwriter.lambda_timeout
-  lambda_env_vars         = var.logwriter.lambda_env_vars
-  lambda_runtime          = var.logwriter.lambda_runtime
-  debug_endpoint          = var.debug_endpoint
-  verbosity               = var.verbosity
-  code_uri                = var.logwriter.code_uri
-  sam_release_version     = try(coalesce(var.logwriter.sam_release_version, var.sam_release_version), null)
+  log_group_name_patterns         = var.logwriter.log_group_name_patterns
+  log_group_name_prefixes         = var.logwriter.log_group_name_prefixes
+  exclude_log_group_name_patterns = var.logwriter.exclude_log_group_name_patterns
+  buffering_interval              = var.logwriter.buffering_interval
+  buffering_size                  = var.logwriter.buffering_size
+  filter_name                     = var.logwriter.filter_name
+  filter_pattern                  = var.logwriter.filter_pattern
+  num_workers                     = var.logwriter.num_workers
+  discovery_rate                  = var.logwriter.discovery_rate
+  lambda_memory_size              = var.logwriter.lambda_memory_size
+  lambda_timeout                  = var.logwriter.lambda_timeout
+  lambda_env_vars                 = var.logwriter.lambda_env_vars
+  lambda_runtime                  = var.logwriter.lambda_runtime
+  debug_endpoint                  = var.debug_endpoint
+  verbosity                       = var.verbosity
+  code_uri                        = var.logwriter.code_uri
+  sam_release_version             = try(coalesce(var.logwriter.sam_release_version, var.sam_release_version), null)
 
   depends_on = [aws_s3_bucket_notification.this]
 }
