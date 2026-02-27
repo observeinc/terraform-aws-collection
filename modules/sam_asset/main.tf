82dfdaaf53fe169a47556690ee86c4ecf324e72d
Justin Daines <168476932+obs-gh-justindaines@users.noreply.github.com>
2026-02-27 08:02:40 -0500

    feat: Update dependencies

--- modules/sam_asset/main.tf
+++ modules/sam_asset/main.tf
@@ -1,6 +1,6 @@
 locals {
   # updatecli will bump this value when new releases become available
-  latest_version = "2.5.0"
+  latest_version = "2.10.3"
 
   release_version = var.release_version != "" ? var.release_version : local.latest_version
 }
